04ea529e439bfde79a2b0228ee1393387a80e52c
eratchacha <papa021326@naver.com>
2025-02-12 10:47:26 +0900

    feat: add Choice Class

---
+++ src/dev/hooboolhoo/model/Choice.java
@@ -0,0 +1,27 @@
+package dev.hooboolhoo.model;
+
+public class Choice {
+    private String name;
+    private int count;
+
+    public Choice(String name, int count) {
+        this.name = name;
+        this.count = count;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public void setCount() {
+        count++;
+    }
+}
