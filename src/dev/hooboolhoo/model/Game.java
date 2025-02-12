c3aff51e57e764d2b25377e47bc13a5a5fb5c77b
young chan <119031366+yyc0026@users.noreply.github.com>
2025-02-12 11:29:10 +0900

    7 feat game class (#9)
    * feat: game class
    
    * 닉네임 수정
    
    * 필드명 수정 불필요 메서드 삭제

---
+++ src/dev/hooboolhoo/model/Game.java
@@ -0,0 +1,90 @@
+package dev.hooboolhoo.model;
+
+import java.util.List;
+
+public class Game {
+
+	private String author;
+	private String category;
+	
+	private String gameThumbnail;
+	
+	private String gameTitle;
+	private String subTitle;
+	
+	List<Choice> choice;
+	List<Comments> comments;
+
+	private int totalCount;
+
+	public String getGameId() {
+		
+	}
+
+	public String getNickName() {
+		return author;
+	}
+	
+	public void setNickName(String author) {
+		this.author = author;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public String getGamethumbnail() {
+		return gameThumbnail;
+	}
+
+	public void setGamethumbnail(String gamethumbnail) {
+		this.gameThumbnail = gamethumbnail;
+	}
+
+	public String getGameTitle() {
+		return gameTitle;
+	}
+
+	public void setGameTitle(String gameTitle) {
+		this.gameTitle = gameTitle;
+	}
+
+	public String getSubTitle() {
+		return subTitle;
+	}
+
+	public void setSubTitle(String subTitle) {
+		this.subTitle = subTitle;
+	}
+
+	public List<Choice> getChoice() {
+		return choice;
+	}
+	
+	public List<Comments> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<Comments> comments) {
+		this.comments = comments;
+	}
+	public void setChoice(List<Choice> choice) {
+		this.choice = choice;
+	}
+
+
+	public int getTotalCount() {
+		return totalCount;
+	}
+
+	public void setTotalCount(int totalCount) {
+		this.totalCount = totalCount;
+	}
+	
+	
+	
+}
